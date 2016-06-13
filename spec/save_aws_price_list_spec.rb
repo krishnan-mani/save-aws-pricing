28418810df59609d041945f4b1533e69b3463a87
Krishnan Mani <km@krishnanm.com>
2016-06-13 12:02:07 +0530

    Preparing to add offer term codes

--- spec/save_aws_price_list_spec.rb
+++ spec/save_aws_price_list_spec.rb
@@ -3,17 +3,17 @@ require 'mongo'
 
 require_relative '../lib/save_aws_price_list'
 
-RSpec.describe "save AWS price list" do
+RSpec.describe "save AWS price list from offer-index file" do
 
   db_url = "mongodb://127.0.0.1:27017/test_save_aws_pricing"
+  offer_index_filename = File.join(File.dirname(__FILE__), 'resources', 'AmazonRoute53_offer-index.json')
+  offer_index_json = JSON.parse(File.read(offer_index_filename))
 
   before(:each) do
     Mongo::Client.new(db_url).database.drop
   end
 
-  it "saves offer code, sku by version for the service as specified in the offer-index file" do
-    offer_index_filename = File.join(File.dirname(__FILE__), 'resources', 'AmazonRoute53_offer-index.json')
-    offer_index_json = JSON.parse(File.read(offer_index_filename))
+  it "saves offer code, sku by version for the service" do
     _offer_code = offer_index_json["offerCode"]
     _version = offer_index_json["version"]
 
@@ -30,6 +30,10 @@ RSpec.describe "save AWS price list" do
     expect(found_sku["offerCode"]).to eq(_offer_code)
     expect(found_sku["productFamily"]).to eq(first_sku["productFamily"])
     expect(found_sku["attributes"]).to eq(first_sku["attributes"])
+  end
+
+  it "saves offer code, terms and offer term code by version for the service" do
+    :pending
   end
 
 end
